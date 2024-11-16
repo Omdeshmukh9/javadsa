6893d7ff7f93ceb056a120c109e62456d1b852ee
Omdeshmukh9 <omdesh89@gmail.com>
2024-11-16 15:04:09 +0530

    2

--- Loops.java
+++ Loops.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int counter = 0;
 
-        while (counter <= 10) {
+        while (counter <= n) {
             System.out.println(counter);
             counter++;
         }
