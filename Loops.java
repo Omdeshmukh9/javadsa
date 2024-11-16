88523e01f5e9a2594939ddee0f1a098b511c24f7
Omdeshmukh9 <omdesh89@gmail.com>
2024-11-16 16:23:15 +0530

    primne

--- Loops.java
+++ Loops.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Loops {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int i = 0;
-        int sum = 0 ;
-        while (i <= n) {
-           
-            sum = sum + i ;
-            i++;    
-        }
-        System.out.println(sum);
+        do {
+            System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+            if (n % 10 == 0) {
+                continue;
+            }
+            System.out.println("enterered num is : " +n);
+        } while (true);
     }
-
 }
