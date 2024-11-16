cf9ab976c76c6d83724ed482b0740f8090a3e621
Omdeshmukh9 <omdesh89@gmail.com>
2024-11-16 14:51:21 +0530

    2nd

--- Loops.java
+++ Loops.java
@@ -2,8 +2,8 @@ public class Loops {
     public static void main(String[] args) {
         int counter = 0;
 
-        while (counter < 100) {
-            System.out.println("Hello world");
+        while (counter <= 10) {
+            System.out.println(counter);
             counter++;
         }
 
